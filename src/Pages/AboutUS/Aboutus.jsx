263396d6ae5f3a2246c2ccf24f8bb8be92107a82
abinandhan2003 <abinandhan77@gmail.com>
2024-11-27 17:33:50 +0530

    firdsfsdfdstasdnver

--- src/Pages/AboutUS/Aboutus.jsx
+++ src/Pages/AboutUS/Aboutus.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hlosclsvg from './img/1.png'
+import Hlosclsvg from './img/3.png'
 import { CiFacebook } from "react-icons/ci";
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -14,10 +14,10 @@ function Aboutus() {
 
             <h1 className='text-3xl'>About Us</h1>
 
-            <div className='bg-white rounded-lg py-5 dark:bg-[#1f1f1f] h-[75vh] mt-4 flex flex-col items-center'>
+            <div className='bg-white rounded-lg py-5 dark:bg-[#1f1f1f] md:h-[75vh] h-full mt-4 flex flex-col items-center'>
 
                 <img src={Hlosclsvg} alt="HloScl_Logo" className='w-36 rounded-full dark:border border-gray-700 bg-gray-100 dark:bg-transparent' />
-                {/* <div className='text-3xl mt-2'>HLOSCL</div> */}
+                <div className='text-3xl mt-2 font-semibold'> HLOSCL </div>
 
                 <div className='mt-4 md:w-[50%] w-[90%] text-center'>
                     Welcome to Hloskl by Nexcap! We strive to make education management simple and efficient for schools, teachers, students, and parents. Our app is designed to streamline day-to-day school operations, improve communication, and ensure a seamless experience for everyone involved. With Hloskl, managing everything from attendance to exams, homework to fees, is just a few clicks away. Join us in transforming education through technology and innovation.
@@ -30,11 +30,13 @@ function Aboutus() {
                     NEXCAP
                 </div>
                 <div className='flex mt-2 gap-2'>
+               
                   <a href="https://www.facebook.com/people/nexcap_official/100090766908092/?mibextid=ZbWKwL">  <CiFacebook  className='text-xl' /> </a>
                   <a href="https://www.youtube.com/@nexcapofficial">  <FaYoutube className='text-xl' /> </a>
                   <a href="https://x.com/nexcap_official?t=Rki-EdWeKDUN3vnbsPyb7g&s=09"> <FaXTwitter className='text-xl' /> </a>
                   <a href="https://www.instagram.com/nexcap_official?igsh=M3A3eWhmNHN3ZWlw">  <FaInstagram className='text-xl' /> </a>
                   <a href="https://www.linkedin.com/company/nexcapdev/">  <FaLinkedinIn className='text-xl' /> </a>
+               
                 </div>
 
                 {/* <div className='md:block hidden w-full h-14'>
